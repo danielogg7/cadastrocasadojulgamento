67aa906960aa677fd134881f3d49e309eeac2bc1
Carlos Daniel Cardoso Muniz Ogg dos Santos <danielogg94@gmail.com>
2025-07-16 12:18:31 -0300

    Mudança na conexão do banco de dados

--- casadojulgamento/__init__.py
+++ casadojulgamento/__init__.py
@@ -5,10 +5,12 @@ import os
 
 app = Flask(__name__)
 
-if os.getenv('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///casadojulgamento.db'
+# if os.getenv('DATABASE_URL'):
+#     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# else:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///casadojulgamento.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:Jesus28xp!@cadastrocj.cxqymaayqcnu.us-east-2.rds.amazonaws.com:3306/cadastrocj'
 
 app.config['SECRET_KEY'] = '06f8b60c12dfb8105f0b51e54ff28dfc'
 
